771561d61a2ce604e4f133694a12a068dfc39d96
Dieg0SIERRA <140528129+Dieg0SIERRA@users.noreply.github.com>
2023-11-17 17:11:45 +0100

    Update basics methods

--- game/src/spaceShip.cpp
+++ game/src/spaceShip.cpp
@@ -8,23 +8,50 @@
 
 Spaceship::Spaceship() : BasicElement()
 {
-
+  setX(37);
+  setY(30);
+  setHealth('s');
+  setLifes('s');
 }
 
 int Spaceship::getLifes() {    return m_lifes;    }
 void Spaceship::setLifes(char value)
 {   
-
+    if(value == '+')    ++m_lifes;
+    else if(value == '-')    --m_lifes;
+    else if(value == 's')    m_lifes = MAX_LIFE; 
 }
 
 void Spaceship::draw() 
 {
+    int valueY = getY();
+    int valueX = getX();
 
+    gotoxy(valueX, valueY);      printf("  %c", 30);
+    gotoxy(valueX, valueY+1);    printf(" %c%c%c", 40, 207, 41);
+    gotoxy(valueX, valueY+2);    printf("%c%c %c%c", 30, 190, 190, 30);
+    gotoxy(0, 34);
 }
 
 void Spaceship::move()
 {
+    int valueY = getY();
+    int valueX = getX();
+    char touch;
 
+    if (kbhit())    //Fonction to detect if a touch was 
+    {
+        touch = getch();
+        erase();
+        if (touch == 'q' && valueX > 3)      valueX--;
+        if (touch == 'd' && valueX+6 < 77)   valueX++;
+        if (touch == 'z' && valueY > 4)      valueY--;
+        if (touch == 's' && valueY+3 < 33)   valueY++;
+        
+        setX(valueX);
+        setY(valueY);                       
+        draw();
+    }
 }
 
 void Spaceship::collisionDetection(int x, int y) 
@@ -32,7 +59,12 @@ void Spaceship::collisionDetection(int x, int y)
 
 void Spaceship::erase() 
 {
+    int valueY = getY();
+    int valueX = getX();
 
+    gotoxy(valueX, valueY);      printf("         ", 30);
+    gotoxy(valueX, valueY+1);    printf("         ", 30);
+    gotoxy(valueX, valueY+2);    printf("         ", 30);
 }
 
 
