d0456676a01629f76e756e42da1aa1152e2938d2
Dieg0SIERRA <140528129+Dieg0SIERRA@users.noreply.github.com>
2023-11-17 16:59:30 +0100

    adding define const and collision enum

--- game/inc/tools.h
+++ game/inc/tools.h
@@ -15,7 +15,27 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <ctime>
+#include <chrono>
 
+#define LIM_LEFT 3
+#define LIM_RIGHT 76
+#define RIGHT   77
+#define LEFT    75
+#define UP      72
+#define DOWN    80
+#define POINT   5
+#define MAX_HEALTH 2
+#define MAX_LIFE 2
+
+enum Collision
+{
+    NONE    = 0,
+    Asteroid_Destroyed,
+    Spaceship_impacted,
+    Lost_life,
+    Game_Over
+};
 
 void gotoxy(int x, int y);
 void ConsoleColor(int n);
