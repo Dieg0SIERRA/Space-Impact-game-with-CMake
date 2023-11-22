38c374683a2046fa9ba1ff37d61ab799fbd2baff
Dieg0SIERRA <140528129+Dieg0SIERRA@users.noreply.github.com>
2023-11-22 19:01:55 +0100

    Adding implementation of constructor and  move method bullet.cpp

--- game/src/bullet.cpp
+++ game/src/bullet.cpp
@@ -8,12 +8,22 @@
 
 Bullet::Bullet(int x, int y) : BasicElement()
 {
-
+  setX(x);
+  setY(y);
 }
 
 bool Bullet::move()
 {
+    bool bulletOut = false;
+    int valueY = getY();
+    int valueX = getX();
+    gotoxy(valueX, valueY);      printf("  ");
 
+    if (valueY > 4) {   setY(--valueY);  }
+    else if (valueY == 4)    {  bulletOut = true;  }   
+    
+    gotoxy(valueX, valueY);      printf("*");
+    return bulletOut;
 }
 
 void Bullet::erase()
