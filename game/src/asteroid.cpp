2c050c0893af9dd48388eb9a912fb5f7c9306308
Dieg0SIERRA <140528129+Dieg0SIERRA@users.noreply.github.com>
2023-11-22 18:55:25 +0100

    Adding constructor an draw method asteroid.cpp

--- game/src/asteroid.cpp
+++ game/src/asteroid.cpp
@@ -8,12 +8,13 @@
 
 Asteroid::Asteroid(int x, int y) : BasicElement()
 {
-
+  setX(x);
+  setY(y);  
 }
 
 void Asteroid::draw(int x, int y)
 {
-
+  gotoxy(x, y);      printf(" %c", 184);
 }
 
 void Asteroid::move() 
